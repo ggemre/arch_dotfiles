c8ee1a04c561e7b4c2ccd167ce59169f237a234e
ggemre <80432807+ggemre@users.noreply.github.com>
2022-06-02 18:54:32 -0400

    Update lock.sh

--- Configs/Scripts/lock.sh
+++ Configs/Scripts/lock.sh
@@ -1,5 +1,5 @@
-  1 #!/bin/sh
-  2
-  3 i3-msg "exec alacritty -e fish -c 'sleep 1 && cmatrix -s -u10'"
-  4 sleep 1
-  5 i3-msg "fullscreen enable"
+#!/bin/sh
+
+i3-msg "exec alacritty -e fish -c 'sleep 1 && cmatrix -s -u10'"
+sleep 1
+i3-msg "fullscreen enable"
