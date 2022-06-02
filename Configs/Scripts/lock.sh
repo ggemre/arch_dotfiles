56dd7b532afea9b2d4719325e94bcc9a29616a2c
ggemre <80432807+ggemre@users.noreply.github.com>
2022-06-02 18:51:05 -0400

    Fixes fullscreen toggle

--- Configs/Scripts/lock.sh
+++ Configs/Scripts/lock.sh
@@ -1,6 +1,5 @@
-#!/bin/sh
-
-SCREEN_SAVER="alacritty --command cmatrix -s -u10"
-i3-msg "exec $SCREEN_SAVER"
-sleep 1
-i3-msg "fullscreen enable"
+  1 #!/bin/sh
+  2
+  3 i3-msg "exec alacritty -e fish -c 'sleep 1 && cmatrix -s -u10'"
+  4 sleep 1
+  5 i3-msg "fullscreen enable"
